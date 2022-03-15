60d86a3dd868bad88ce4b35f67188be11d8fb00f
juniorvansuita <Vansuita1324>
2022-03-15 10:36:04 -0300

    robbery-imgs

--- src/pages/market.js
+++ src/pages/market.js
@@ -1,14 +1,19 @@
 const MarketMecanics = require("../mecanics/market");
+const PlayerController = require("../controller/player");
 
 module.exports = class MarketPage {
 
     constructor() {
         this.marketMecanics = new MarketMecanics();
+        this.playerController = new PlayerController();
     }
 
     bind(app) {
 
 
+        app.get('/market', (req, res) => {
+            this.playerController.get(req.session.playerId).then(player => res.render('pages/market', { player }));
+        });
 
 
     }
